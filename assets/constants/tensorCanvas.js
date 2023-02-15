9cb2d75b6dded41e38ca94a58842b6dfd04f49af
DanielSoltysiak <72917460+DanielSoltysiak@users.noreply.github.com>
2023-02-15 10:10:46 +0100

    scale chnge

--- assets/constants/tensorCanvas.js
+++ assets/constants/tensorCanvas.js
@@ -1,5 +1,6 @@
 // export const scale = 0.0625
-export const scale = 0.03125
+// export const scale = 0.03125
+export const scale = 0.015625
 export const multipier = 1/scale
 const tensorCanvas = {};
 tensorCanvas.width = 1024 * scale
