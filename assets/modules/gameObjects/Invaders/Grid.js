4c944606d7f30ab4a97a0d200140a757d13d3226
DanielSoltysiak <72917460+DanielSoltysiak@users.noreply.github.com>
2023-03-01 21:25:01 +0100

    invaders nerf

--- assets/modules/gameObjects/Invaders/Grid.js
+++ assets/modules/gameObjects/Invaders/Grid.js
@@ -9,14 +9,14 @@ export default class Grid {
         }
 
         this.velocity = {
-            x: 2 * scale,
+            x: 1 * scale,
             y: 0
         }
 
         this.invaders = []
 
-        const columns = Math.floor(Math.random() * 10 + 5)
-        const rows = Math.floor(Math.random() * 5 + 2)
+        const columns = Math.floor(Math.random() * 10 + 3)
+        const rows = Math.floor(Math.random() * 5 + 1)
 
         this.width = columns * 30 * scale
         this.height = rows * 30 * scale
