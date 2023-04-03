525b8d65ef49f71748e17b63fb102ce4e5a63745
DanielSoltysiak <72917460+DanielSoltysiak@users.noreply.github.com>
2023-04-03 08:33:36 +0200

    K2, D4, N0.05

--- assets/spaceInvadersTensor.js
+++ assets/spaceInvadersTensor.js
@@ -8,9 +8,9 @@ import invadersAction from './modules/actions/invadersAction.js';
 import playerAction from './modules/actions/playerAction.js';
 import invadersSpawn from './modules/actions/invadersSpawn.js';
 
-export const NO_KILL_REWARD = -0.03;
-export const KILL_REWARD = 3;
-export const DEATH_REWARD = -3;
+export const NO_KILL_REWARD = -0.05;
+export const KILL_REWARD = 2;
+export const DEATH_REWARD = -4;
 
 export const ACTION_SHOOT = 0;
 export const ACTION_TURN_RIGHT = 1;
